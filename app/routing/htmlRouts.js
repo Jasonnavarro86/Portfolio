1576e86728ddfcf682cb7349d6b2040adcedee91
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-11-15 23:42:42 -0500

    75% Done. Added Images and partial text.

---
+++ app/routing/htmlRouts.js
@@ -0,0 +1,15 @@
+// Here I set my function to send html files in our main server.js.
+
+// function routeOne(app, __dirname, path) {
+    
+//         // Home page
+//         app.get("/", function (req, res) {
+            
+//             res.sendFile(path.join(__dirname + "/app/public/index.html"))
+//         })
+    
+//     }
+     
+//     // Exporting Function
+//     module.exports = routeOne;
+    
